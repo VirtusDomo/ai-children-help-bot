40517ee5e99cd27d28d07bb2641b13db0fa81678
Sara Shao <Sara.Shao@metronetinc.com>
2021-10-02 16:14:37 -0400

    talking function

--- talking-anim.py
+++ talking-anim.py
@@ -1,14 +1,10 @@
-<<<<<<< HEAD
 import pygame as pg
+import time
 
 pg.init()
 
-white = (255,255,255)
 black = (0,0,0)
-
-red = (255,0,0)
-green = (0,255,0)
-blue = (0,0,255)
+white = (255,255,255)
 
 eyesImg = pg.image.load('eyes.jpg')
 faceImg = pg.image.load('default-face.jpg')
@@ -17,39 +13,26 @@ talkingImg = pg.image.load('eyes-mouth.jpg')
 clock = pg.time.Clock()
 gameDisplay = pg.display.set_mode((537,403))
 
-while True:
+def talking(seconds):
+    t_end = time.time() + seconds
+    while time.time() < t_end:
+        gameDisplay.blit(talkingImg, (0,0))
+        pg.display.flip()
+        clock.tick(4)
+        gameDisplay.blit(faceImg, (0,0))
+        pg.display.flip()
+        clock.tick(4)
+    return
+
+
+close = time.time() + 60
+
+while time.time() < close:
     gameDisplay.fill(black)
     gameDisplay.blit(faceImg, (0,0))
     pg.display.flip()
-    clock.tick(4)
-    gameDisplay.blit(talkingImg, (0,0))
-    pg.display.flip()
-=======
-import pygame as pg
-import time
+    talking(10)
+    clock.tick(1)
 
-pg.init()
-
-white = (255,255,255)
-black = (0,0,0)
-
-red = (255,0,0)
-green = (0,255,0)
-blue = (0,0,255)
-
-eyesImg = pg.image.load('resources/eyes.jpg')
-faceImg = pg.image.load('resources/default-face.jpg')
-talkingImg = pg.image.load('resources/eyes-mouth.jpg')
-
-clock = pg.time.Clock()
-gameDisplay = pg.display.set_mode((537,403))
-
-while True:
-    gameDisplay.fill(black)
-    gameDisplay.blit(faceImg, (0,0))
-    pg.display.flip()
-    clock.tick(4)
-    gameDisplay.blit(talkingImg, (0,0))
-    pg.display.flip()
->>>>>>> f6a0eca2480395faa48338c9a25069f80e83fe0a
-    clock.tick(4)
+pg.quit()
+quit()
