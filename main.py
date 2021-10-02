514f6bd0bc6a21fe1fee5c9e5d9b162ffdd9894c
micpim <59402634+micpim@users.noreply.github.com>
2021-10-02 14:19:44 -0700

    Update main.py

--- main.py
+++ main.py
@@ -6,31 +6,28 @@ import wikipedia
 import pyjokes
 import pyaudio
 
-print('program started')
+print('Program started')
 NAME = 'gizmo'
 # sets up listener
 listener = sr.Recognizer()
 
 # indicates which microphone to use MAKE SURE TO SPECIFY
-mic = sr.Microphone(device_index=0)
+mic = sr.Microphone(device_index = 0)
 engine = pyttsx3.init()
-#engine.setProperty('age')
 voices = engine.getProperty('voices')
-engine.setProperty('voice', voices[0].id)
-engine.say('Hi Bestie')
-engine.say('How can I help you')
-
-# Reminder the user
-time = datetime.datetime.now().strftime('%I:%M %p')
-engine.say('How can I help you')
+# IMPORTANT: Voices[12] is only based on Mike's local machine.
+engine.setProperty('voice', voices[18].id)
+#9 calm voice, 25 laugh, 61, 4, 16 robo sing, 18 base, 24 long song
+# Greet the user, first time activation
+engine.say('Hi Bestie! How can I help you')
 engine.runAndWait()
 
-
-
+# Speaks whatever text is passed into the function
 def talk(text):
     engine.say(text)
     engine.runAndWait()
 
+# Finds the instructions, hands over the resulting text to talk() to be read aloud
 def findTask():
     try:
         with mic as source:
@@ -53,6 +50,8 @@ def findTask():
             if NAME in command:
                 command = command.replace(NAME, '')
                 print(command)
+
+            # If Program cannot hear anything
             else:
                 print("nothing heard")
     except:
@@ -60,31 +59,54 @@ def findTask():
         pass
     return command
 
-# Performs the command accordingly
+# Performs the command accordingly to what the user instructed
 def doTask():
     task = findTask()
     print(task)
+
+    # Play music
     if 'play' in task:
         song = task.replace('play', '')
         talk('playing' + song)
         pywhatkit.playonyt(song)
+
+    # Reports time
     elif 'time' in task:
         time = datetime.datetime.now().strftime('%I:%M %p')
         talk('Hi bestie! the time is ' + time)
+
+    # Defines or describes Wikipedia entry
     elif 'what is ' in task:
         thing = task.replace('what is', '')
         info = wikipedia.summary(thing,2)
         print(info)
         talk(info)
+
+    # Emotional support ( Do you like me?)
     elif 'do you like me' in task:
         talk('You are my best friend')
+
+    elif "I'm scared" in task:
+        talk('Everything will be okay. We are all here for you.')
+
+    # Shuts down the program
     elif 'shut up' in task:
         talk('Okay meanie. I will go now. Goodbye.')
         pass
+
+    # Tells a joke
     elif 'joke' in task:
         talk(pyjokes.get_joke())
+
+    elif "birthday" in task:
+        engine.setProperty('voice', voices[18].id)
+        talk("Happy birthday to you, happy birthday to you, happy birthday dear bestie, happy birthday  to you")
+        engine.setProperty('voice', voices[16].id)
+
+    # Sass
     else:
         talk('Sorry bestie not that smart')
 
+# Use a while loop to keep the inputs going until user requests otherwise
 while True:
     doTask()
