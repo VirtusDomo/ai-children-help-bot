9b84b7145027d4db6e43eea2c8b5f7480f6ccc62
micpim <59402634+micpim@users.noreply.github.com>
2021-10-02 17:20:34 -0700

    Add files via upload

--- main.py
+++ main.py
@@ -6,18 +6,56 @@ import wikipedia
 import pyjokes
 import pyaudio
 from input import Regimen, docVisits
-from bs4 import BeautifulSoup
-import urllib.request as urllib2
-import html2text
+import syllables
+import pygame as pg
+import time
+import sys
 
-# for reading in a web page
-from bs4 import BeautifulSoup
-import urllib2
-import html2text
+pg.init()
+
+black = (0,0,0)
+white = (255,255,255)
+
+faceImg = pg.image.load('default-face.jpg')
+talkingImg = pg.image.load('eyes-mouth.jpg')
+talkingImg2 = pg.image.load('mouth2.jpeg')
+talkingImg3 = pg.image.load('mouth3.jpeg')
+
+clock = pg.time.Clock()
+gameDisplay = pg.display.set_mode((537,403))
 
 
+def talking(seconds):
+    t_end = time.time() + seconds
+    speed = 5
+    while time.time() < t_end:
+        gameDisplay.blit(talkingImg, (0,0))
+        pg.display.flip()
+        clock.tick(speed)
+        gameDisplay.blit(talkingImg2, (0,0))
+        pg.display.flip()
+        clock.tick(speed)
+        gameDisplay.blit(talkingImg2, (0,0))
+        pg.display.flip()
+        clock.tick(speed)
+        gameDisplay.blit(faceImg, (0,0))
+        pg.display.flip()
+        clock.tick(speed)
+    return
 
-print('Program started')
+def speak(syllables):
+    close = time.time() + syllables
+    while time.time() < close:
+        wait = time.time() + 5
+        while time.time() < wait:
+            gameDisplay.blit(faceImg, (0,0))
+            pg.display.flip()
+        talking(syllables/4)
+
+    pg.display.quit()
+    pg.quit()
+    sys.exit()
+
 NAME = 'gizmo'
 # sets up listener
 listener = sr.Recognizer()
@@ -72,7 +110,7 @@ def findTask():
                 print("nothing heard")
     except:
         print('i cannot hear anything')
-        
+        pass
     return command
 
 # Performs the command accordingly to what the user instructed
@@ -83,61 +121,73 @@ def doTask():
     # Play music
     if 'play' in task:
         song = task.replace('play', '')
+        speak(2)
         talk('playing' + song)
         pywhatkit.playonyt(song)
 
     # Reports time
     elif 'time' in task:
         time = datetime.datetime.now().strftime('%I:%M %p')
+        speak(3)
         talk('Hi bestie! the time is ' + time)
 
     # Defines or describes Wikipedia entry
     elif 'what is ' in task:
         thing = task.replace('what is', '')
         info = wikipedia.summary(thing,2)
+        sylls = syllables.estimate(info)
+        speak(sylls)
         print(info)
         talk(info)
 
     # Appointments
     elif 'I forgot my next appointment' in task:
+        speak( 15)
         talk('Not to worry. You are scheduled to see Dr. Chen on December 2nd. That is in a month.')
 
     # Emotional support ( Do you like me?)
     elif 'Do you like me' in task:
+        speak(3)
         talk('You are my best friend')
 
     elif "I'm scared" in task:
+        speak(5)
         talk('Everything will be okay. We are all here for you.')
 
     # Shuts down the program
     elif 'shut up' in task:
+        speak(5)
         talk('Okay meanie. I will go now. Goodbye.')
         pass
 
     # Tells a joke
     elif 'joke' in task:
+        joke = pyjokes.get_joke()
+        sylla = syllables.estimate(joke)
+        speak(sylla)
         talk(pyjokes.get_joke())
 
-    # Education
-    elif ('Can you explain' or 'Tell more more about' or 'What do you mean by' or
-            'down syndrome') in task:
-        soup = BeautifulSoup(urllib2.urlopen('https://www.cdc.gov/ncbddd/birthdefects/' + 'downsyndrome' + '.html').read())
-        talk(soup)
-    
     elif "medicine" in task:
+        sylla = syllables.estimate(Regimen)
+        speak(sylla)
         talk (Regimen)
-    
+
     elif "doctor visit" in task:
+        sylla = syllables.estimate(docVisits)
+        speak(sylla)
         talk (docVisits)
-        
+
     # Birthday
     elif "It's my birthday" in task:
         engine.setProperty('voice', voices[18].id)
+        speak(15)
         talk("Happy birthday to you, happy birthday to you, happy birthday dear bestie, happy birthday to you")
         engine.setProperty('voice', voices[16].id)
+
     # Sass
     else:
-        talk('Sorry could you repeat that')
+        speak(5)
+        talk('Sorry bestie not that smart')
 
 # Use a while loop to keep the inputs going until user requests otherwise
 while True:
