07cb610d7b93729095e69874d0ec882e2f95dd1f
Sara Shao <Sara.Shao@metronetinc.com>
2021-10-02 21:38:00 -0400

    testing audio

--- talking-anim.py
+++ talking-anim.py
@@ -1,6 +1,6 @@
 import pygame as pg
+from pygrame import mixer
 import time
-import sys
 
 pg.init()
 
@@ -13,11 +13,11 @@ talkingImg2 = pg.image.load('mouth2.jpg')
 talkingImg3 = pg.image.load('mouth3.jpg')
 
 clock = pg.time.Clock()
-gameDisplay = pg.display.set_mode((537,403))
 
 def talking(seconds):
     t_end = time.time() + seconds
     speed = 5
+    gameDisplay = pg.display.set_mode((537,403))
     while time.time() < t_end:
         gameDisplay.blit(talkingImg, (0,0))
         pg.display.flip()
@@ -25,7 +25,7 @@ def talking(seconds):
         gameDisplay.blit(talkingImg2, (0,0))
         pg.display.flip()
         clock.tick(speed)
-        gameDisplay.blit(talkingImg2, (0,0))
+        gameDisplay.blit(talkingImg3, (0,0))
         pg.display.flip()
         clock.tick(speed)
         gameDisplay.blit(faceImg, (0,0))
@@ -34,15 +34,15 @@ def talking(seconds):
     return
 
 
-close = time.time() + 40
+mixer.music.load('Ariana Grande - pov (audio).mp3')
+mixer.music.play()
 
+close = time.time() + 20
 while time.time() < close:
-    wait = time.time() + 5
-    while time.time() < wait:
-        gameDisplay.blit(faceImg, (0,0))
-        pg.display.flip()
+    gameDisplay = pg.display.set_mode((537,403))
+    gameDisplay.blit(faceImg, (0,0))
+    pg.display.flip()
     talking(10)
 
-pg.display.quit()
 pg.quit()
-sys.exit()
+quit()
